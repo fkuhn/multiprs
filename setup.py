56b89e0cc66afbc8cd60420ef384df3ec65108f3
Florian Kuhn <fkuhn@posteo.de>
2016-10-04 14:38:30 +0200

    updated requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-    'pyYaml'
+    'pyYaml', 'lxml'
     ]
 setup(
     name="multiprs",
