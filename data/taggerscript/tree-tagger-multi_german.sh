ac60d12b52a18f2142168e985518d2e9b982c767
kuhn <kuhn@ids-mannheim.de>
2016-10-25 14:07:52 +0200

    added data dir

---
+++ data/taggerscript/tree-tagger-multi_german.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+PAR=$1
+
+# Set these paths appropriately
+
+BIN=../TreeTagger/bin
+CMD=../TreeTagger/cmd
+LIB=../TreeTagger/lib
+
+OPTIONS="-token -lemma -sgml -pt-with-lemma"
+
+#TOKENIZER=${CMD}/tokenize.pl
+TOKENIZER=${CMD}/no-tokenize.pl
+TAGGER=${BIN}/tree-tagger
+ABBR_LIST=${LIB}/german-abbreviations-utf8
+#PARFILE=${LIB}/multilitJune21.par
+#PARFILE=${LIB}/multilitOct15.par
+#PARFILE=${LIB}/GermanFEB25.par
+PARFILE=${LIB}/German_we_wn_May20.par
+#LEXFILE=${LIB}/german-lexicon-utf8.txt
+#LEXFILE=${LIB}/October15.lex
+LEXFILE=${LIB}/revisedinputGerFeb25.lex
+FILTER=${CMD}/filter-german-tags
+
+$TOKENIZER -a $ABBR_LIST $* |
+# external lexicon lookup
+perl $CMD/lookup.perl $LEXFILE |
+# tagging
+$TAGGER $OPTIONS $PARFILE  |
+# error correction
+$FILTER
---
+++ data/tags/README.md
@@ -0,0 +1,5 @@
+TAGs
+====
+
+- this folder contains text files with valied pos tags for each used language.
+- note: the person digit must be reconsidered for evaluating the training data later on.
